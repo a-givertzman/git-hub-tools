86cfefb27fd0f3ab751156247e25ace2743851c0
Anton Lobanov <a.givertzman@icloud.com>
2025-12-17 17:43:29 +0300

    fetch-user-prs | sort by date

--- src/fetch-user-prs.py
+++ src/fetch-user-prs.py
@@ -3,6 +3,7 @@ from functools import reduce
 from dateutil import parser
 from file_tools import loadJson, loadToken
 from pagination import fetchPaginated
+from enum import Enum
 
 #
 # Fetching all pull requests from repo of authenticated User
@@ -11,15 +12,52 @@ from pagination import fetchPaginated
 # https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
 #
 
+class ReportSort(Enum):
+    Default = 0
+    Date = 1
+class ReportGroupBy(Enum):
+    User = 1
+    Mounth = 2
+class ReportSettings:
+    def __init__(self, author: list[str], sort: ReportSort, groupBy: ReportGroupBy) -> None:
+        self.author = author
+        self.sort = sort
+        self.groupBy = groupBy
+class PullRequest:
+    def __init__(self, number: str, state: str, title: str, url: str) -> None:
+        self.number = number
+        self.state = state
+        self.title = title
+        self.url = url
+    def __str__(self) -> str:
+        return f"PR {self.number} ;\t {self.state} ;\t {self.title} ;\t {self.url}"
+class Commit:
+    def __init__(self, index: int, pr: PullRequest, date: str, comment: str) -> None:
+        self.index = index
+        self.pr = pr
+        self.date = date
+        self.comment = comment
+    def __str__(self) -> str:
+        # print(f"\tPR {pr['number']} ; {pr['state']}; \t title: {pr['title']}; \t url: {pr['html_url']}")
+        # print(f"\t\tCommit {i} ; date: {commit['commit']['author']['date']}; \t comment: {commit['commit']['message']}")
+        return f"Commit {self.index} ;\t {self.pr.title} ;\t {self.date} ;\t {self.comment}"
+
 
 if __name__ == '__main__':
+    # Report
+    report = ReportSettings(
+        author=[],  # ['a-givertzman', 'Anton Lobanov']
+        sort=ReportSort.Date,
+        groupBy=ReportGroupBy.User,
+    )
+    # Github Request
     visibility = 'all'
     type = 'all'
     sort = 'full_name'
     direction = 'asc'
     page = ''
-    since = '2025-07-01T00:00:00Z'
-    until = '2025-07-09T00:00:00Z'
+    since = '2025-06-01T00:00:00Z'
+    until = '2025-12-31T00:00:00Z'
     sinceDatetime = parser.parse(since)
     untilDatetime = parser.parse(until) + datetime.timedelta(days=1)
     print(f'from: {sinceDatetime} to {untilDatetime}')
@@ -103,34 +141,66 @@ if __name__ == '__main__':
                     commitDatetime = parser.parse(commit['commit']['author']['date'])
                     if sinceDatetime < commitDatetime < untilDatetime:
                         foundCommits.append(commit)
-                        commitAuthor = commit['author']['login']
+                        # print(f"commit: {commit}")
+                        # print(f"commit['author']: {commit['author']}")
+                        if commit['author']:
+                            commitAuthor = commit['author']['login']
+                        else:
+                            commitAuthor = commit['commit']['author']['name']
                         # commit = f"\ti: {i} | {{\"author\": \"{commit['author']['login']}\", \t \"date\": \"{commit['commit']['author']['date']}\"}},"
-                        if commitAuthor in users:
-                            if prNumber in users[commitAuthor]:
-                                users[commitAuthor][prNumber]['commits'].append(commit)
+                        if not report.author or commitAuthor in report.author:
+                            # print(f"commit author: {commitAuthor}")
+                            if commitAuthor in users:
+                                if prNumber in users[commitAuthor]:
+                                    users[commitAuthor][prNumber]['commits'].append(commit)
+                                else:
+                                    users[commitAuthor][prNumber] = {
+                                        'pr': pr,
+                                        'commits': [commit],
+                                    }
                             else:
-                                users[commitAuthor][prNumber] = {
-                                    'pr': pr,
-                                    'commits': [commit],
+                                users[commitAuthor] = {
+                                    prNumber: {
+                                        'pr': pr,
+                                        'commits': [commit],
+                                    },
                                 }
                         else:
-                            users[commitAuthor] = {
-                                prNumber: {
-                                    'pr': pr,
-                                    'commits': [commit],
-                                },
-                            }
+                            # print(f"skipped commit of author: {commitAuthor}")
+                            pass
     # pprint(users)
     if users:
-        for user, userPrs in users.items():
-            print(f'\nAuthor: {user}')
-            for _, userPr in userPrs.items():
-                pr = userPr['pr']
-                if 'commits' in userPr:
-                    print(f"\tPR {pr['number']} | {pr['state']}, \t title: {pr['title']}, \t url: {pr['html_url']},")
-                    # print(f"\tPR {pr['number']} | user: {pr['user']['login']}, \t state: {pr['state']}, \t title: {pr['title']}, \t url: {pr['html_url']},")
-                    for i, commit in enumerate(userPr['commits']):
-                        print(f"\t\tCommit {i} | date: {commit['commit']['author']['date']}, \t comment: {commit['commit']['message']},")
-                        # print(f"\t\tCommit {i} | {{author: {commit['author']['login']}, \t date: {commit['commit']['author']['date']}, \t comment: {commit['commit']['message']}}},")
+        if report.groupBy == ReportGroupBy.User:
+            if report.sort == ReportSort.Date:
+                for user, userPrs in users.items():
+                    print(f'\nAuthor: {user}')
+                    commits: list[Commit] = []
+                    for _, userPr in userPrs.items():
+                        pr = userPr['pr']
+                        if 'commits' in userPr:
+                            pr = PullRequest(pr['number'], pr['state'], pr['title'], pr['html_url'])
+                            for i, commit in enumerate(userPr['commits']):
+                                commits.append(
+                                    Commit(
+                                        index=i,
+                                        pr=pr,
+                                        date=commit['commit']['author']['date'],
+                                        comment=commit['commit']['message'],
+                                    )
+                                )
+                    commits.sort(key=lambda c: c.date)
+                    for c in commits:
+                        print(c)
+            else:
+                for user, userPrs in users.items():
+                    print(f'\nAuthor: {user}')
+                    for _, userPr in userPrs.items():
+                        pr = userPr['pr']
+                        if 'commits' in userPr:
+                            print(f"\tPR {pr['number']} ; {pr['state']}; \t title: {pr['title']}; \t url: {pr['html_url']}")
+                            # print(f"\tPR {pr['number']} | user: {pr['user']['login']}, \t state: {pr['state']}, \t title: {pr['title']}, \t url: {pr['html_url']},")
+                            for i, commit in enumerate(userPr['commits']):
+                                print(f"\t\tCommit {i} ; date: {commit['commit']['author']['date']}; \t comment: {commit['commit']['message']}")
+                                # print(f"\t\tCommit {i} | {{author: {commit['author']['login']}, \t date: {commit['commit']['author']['date']}, \t comment: {commit['commit']['message']}}},")
 
 
